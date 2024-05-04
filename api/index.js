d79bffceaa8553eb0203c1890a735324ef09637c
Shisode-S <shisodeswapnil2022@gmail.com>
2024-05-04 16:19:22 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,16 +2,24 @@ import express  from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
-})
+}) .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 const app = express();
 
+app.use(express.json())
+
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
 
 app.listen(3000 , () => {
     console.log('Server listening on port 3000!');
